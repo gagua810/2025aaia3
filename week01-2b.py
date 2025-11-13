74de0ee440cfdccb2a7e60763209ca2c88a9d33c
gagua810 <jimmyjinjia@gmail.com>
2025-11-13 17:21:42 +0800

    week01

--- week01-2b.py
+++
@@ -1,6 +0,0 @@
-#week01-2b.py 使用Python 的版本1 使用 .find()函式
-#LeetCode 28.
-#在haystack 乾稻草堆裡找到needle 針（大海撈針）
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
